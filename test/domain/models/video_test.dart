35960ea734f5afca6dd7720343f7a6f43c4f9658
Aldi <aldi.app@qubisa.com>
2023-04-14 23:43:08 +0700

    [main] adding test case for constructor in video

--- test/domain/models/video_test.dart
+++ test/domain/models/video_test.dart
@@ -2,12 +2,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:video_player_app/domain/models/models.dart';
 
 void main() {
-  test("FromJson returns value correctly", () async {
-    int trackId = 1445738215;
-    String trackName = "You And Your Heart";
-    int trackTimeMillis = 5000;
-    String previewUrl = "testPreviewUrl";
+  const int trackId = 1445738215;
+  const String trackName = "You And Your Heart";
+  const int trackTimeMillis = 5000;
+  const String previewUrl = "testPreviewUrl";
 
+  test("Video constructor returns value correctly", () async {
+    Video video = Video(
+      trackId: trackId,
+      trackName: trackName,
+      trackTimeMillis: trackTimeMillis,
+      previewUrl: previewUrl,
+    );
+
+    expect(video.trackId, trackId);
+    expect(video.trackName, trackName);
+    expect(video.trackTimeMillis, trackTimeMillis);
+    expect(video.previewUrl, previewUrl);
+
+    expect(video.props, [trackId, trackName, trackTimeMillis, previewUrl]);
+  });
+
+  test("Video.fromJson() returns value correctly", () async {
     Map<String, dynamic> videoMap = {
       "trackId": trackId,
       "trackName": trackName,
