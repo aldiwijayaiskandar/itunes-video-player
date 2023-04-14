3812d5f689b07ffe0c9a674f50cbedf3c3172dc6
Aldi <aldi.app@qubisa.com>
2023-04-14 18:46:50 +0700

    [main] create video repository

---
+++ lib/data/repositories/video.dart
@@ -0,0 +1,20 @@
+import 'package:video_player_app/data/services/api/api.dart';
+import 'package:video_player_app/domain/models/models.dart';
+import 'package:video_player_app/domain/repositories/repositories.dart';
+
+class VideoRepoImpl extends VideoRepo {
+  final ApiService apiService;
+
+  VideoRepoImpl(this.apiService);
+
+  @override
+  Future<List<Video>> getVideoList() async {
+    final result = await apiService.getApi(
+      url: "/search?term=jack+johnson&entity=musicVideo",
+    );
+
+    return (result.data['results'] as List)
+        .map((x) => Video.fromJson(x))
+        .toList();
+  }
+}
---
+++ test/data/video_test.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:video_player_app/data/repositories/repositories.dart';
+import 'package:video_player_app/data/services/services.dart';
+import 'package:video_player_app/domain/repositories/repositories.dart';
+
+import '../constants/video.dart';
+import '../mocks/api.dart';
+
+void main() {
+  final ApiService api = MockApiService();
+  late VideoRepo repo;
+
+  setUp(() {
+    repo = VideoRepoImpl(api);
+  });
+
+  test("Get video list successful", () async {
+    when(
+      () => api.getApi(url: "/search?term=jack+johnson&entity=musicVideo"),
+    ).thenAnswer(
+      (_) => Future(
+        () => Response(
+          requestOptions: RequestOptions(),
+          data: videoJson,
+        ),
+      ),
+    );
+
+    final result = await repo.getVideoList();
+
+    expect(result, videoList);
+  });
+}
